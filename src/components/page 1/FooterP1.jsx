fe09dedbd60c59b5ce68b364f75c22cb0dfc4e7e
Najibmansouur <najibmansour2005@gmail.com>
2024-01-14 17:59:57 +0200

    fixed footer

--- src/components/page 1/FooterP1.jsx
+++ src/components/page 1/FooterP1.jsx
@@ -3,7 +3,7 @@ import DownArrow from "../svgs/symbols/downArrow";
 
 const FooterP1 = () => {
   return (
-    <div className="w-full text-primary-content flex flex-col  mb-16 justify-end items-center h-full ">
+    <div className="w-full h-full text-primary-content flex flex-col  mb-16 justify-end items-center  ">
       <div className=" animate-bounce  flex flex-col items-center">
         <p className="lg:text-2xl text-xl mb-4">исследовать</p>
         <DownArrow className="lg:w-14 w-10" />
