f8cd6114203b45a7752ee7f3dfb2ac9a93f33184
Najibmansouur <najibmansour2005@gmail.com>
2024-01-20 02:08:05 +0200

    changed color of page 2

--- src/components/Pages/page2.jsx
+++ src/components/Pages/page2.jsx
@@ -4,7 +4,7 @@ import GridElement from "../page 2/gridElement";
 const Page2 = () => {
   return (
     <div className=" flex justify-center items-center w-screen h-screen flex-col ">
-      <p className="text-5xl w-96">
+      <p className="text-5xl w-96 text-secondary">
         <span className="font-semibold ">Наша миссия</span> освободить время
         клиентов и увеличить их прибыль
       </p>
