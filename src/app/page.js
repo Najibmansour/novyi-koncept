9ac2ab7272ad630a92d744aabe37132d4417aeb9
michael-debs <100143939+michael-debs@users.noreply.github.com>
2024-01-21 19:03:42 +0200

    fixed page 4 not found

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Page1 from "@/components/Pages/page1";
 import Page2 from "@/components/Pages/page2";
 import Page3 from "@/components/Pages/page3";
+import Page4 from "@/components/Pages/page4";
 
 export default function Home() {
   return (
