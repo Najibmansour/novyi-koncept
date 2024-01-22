bbfe8818d3010c2b99b72e98a4c334f42102d52a
michael-debs <100143939+michael-debs@users.noreply.github.com>
2024-01-22 19:12:20 +0200

    added page 5

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Page1 from "@/components/Pages/page1";
 import Page2 from "@/components/Pages/page2";
 import Page3 from "@/components/Pages/page3";
 import Page4 from "@/components/Pages/page4";
+import Page5 from "@/components/Pages/page5";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Page2 />
       <Page3 />
       <Page4 />
+      <Page5 />
     </div>
   );
 }
