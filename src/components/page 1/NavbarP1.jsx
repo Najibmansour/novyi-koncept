863c214e57e340552f630c662575e26f62f691aa
Najibmansouur <najibmansour2005@gmail.com>
2024-01-22 19:05:48 +0200

    Update NavbarP1.jsx

--- src/components/page 1/NavbarP1.jsx
+++ src/components/page 1/NavbarP1.jsx
@@ -3,7 +3,7 @@ import Logo from "../svgs/logo/Logo";
 
 const NavbarP1 = () => {
   return (
-    <div className="navbar  lg:h-48 h-36  lg:pl-36 pl-14  items-stretch">
+    <div className="navbar lg:h-48 h-36 lg:pl-36 pl-14  items-stretch">
       <Logo className="lg:w-20 w-16" />
     </div>
   );
