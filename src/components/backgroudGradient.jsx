c960462be84b3c1f43b77f8e8bf97974036c2e8d
Najibmansouur <najibmansour2005@gmail.com>
2024-02-23 21:28:24 +0200

    idk gradient

--- src/components/backgroudGradient.jsx
+++ src/components/backgroudGradient.jsx
@@ -5,11 +5,11 @@ const BackgroudGradient = () => {
     <div className="relative h-[550svh] w-screen overflow-hidden bg-gradient-to-r from-pink-700 to-fuchsia-700 blur-[250px] ">
       <div className="shape1 absolute -right-[20%] bottom-[50%] h-[150vh] w-[150vh] rounded-full bg-[#996AA4] mix-blend-overlay" />
       <div className="shape2 absolute -right-[20%]  -top-[10%] h-[150vh] w-[250vh] rounded-full bg-[#2b2264] mix-blend-overlay" />
-      <div className="shape3 absolute -left-[30%] bottom-[0%] h-[150vh] w-[150vh] rounded-full bg-[#DF5C61] mix-blend-overlay" />
+      <div className="shape3 absolute -left-[50%] bottom-[0%] h-[150vh] w-[150vh] rounded-full bg-[#DF5C61] mix-blend-overlay lg:-left-[30%]" />
       <div className="shape4 absolute -bottom-[15%] -right-[20%] h-[150vh] w-[150vh] rounded-full bg-[#852D90] mix-blend-overlay" />
       <div className="shape1 absolute -right-[20%]  bottom-[14%] h-[150vh] w-[150vh] rounded-full bg-[#2D2460] mix-blend-overlay" />
       <div className="shape2 absolute -right-[20%] bottom-[50%] h-[150vh] w-[150vh] rounded-full bg-[#986FAF] mix-blend-overlay" />
-      <div className="shape4 absolute -left-[50%] bottom-[33%] h-[200vh] w-[200vh] rounded-full bg-[#FF7765] mix-blend-overlay" />
+      <div className="shape4 absolute -left-[70%] bottom-[33%] h-[200vh] w-[200vh] rounded-full bg-[#FF7765] mix-blend-overlay lg:-left-[50%]" />
       <div className="shape5 absolute -left-[20%] -top-[10%] h-[200vh] w-[150vh] rounded-full bg-[#B24995] mix-blend-overlay" />
     </div>
   );
